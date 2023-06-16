26504f1ad218b00b99d9b0cfeaa1a0e3aeb2ec1c
Stepan Andreev <stepan10andreev@gmail.com>
2023-06-16 17:05:52 +0300

    some render conditions fixes in CardList

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,19 +1,17 @@
 import { FC, useEffect, useState } from "react";
 import styles from './CardList.module.scss';
 import { Card } from "./Card/Card";
-import useSWR from 'swr';
 import axios from "axios";
 import { getCookie } from "cookies-next";
-import { createModuleResolutionCache } from "typescript";
 import useSWRInfinite from 'swr/infinite'
-import { GridLoader, PropagateLoader } from "react-spinners";
+import { PropagateLoader } from "react-spinners";
 
 // type ICardListsProps = Pick<IHomePageProps, 'postsData'>
 
 const fetchPosts = async (url: string) => {
   const token = getCookie('token')
 
-   const {data} = await axios.get(`https://oauth.reddit.com${url}`, {
+  const {data} = await axios.get(`https://oauth.reddit.com${url}`, {
     headers: { Authorization: `bearer ${token}` },
   })
 
@@ -24,29 +22,47 @@ const getKey = (pageIndex: number, previousPageData: { children: any; after: any
   // // если достигнут конец
   if (previousPageData && !previousPageData.children) return null
 
-
   if (pageIndex === 0) return `/r/all/hot.json?limit=10`
 
   return `/r/all/hot.json?limit=10&after=${previousPageData.after}`
 }
 
+
 export const CardList: FC = () =>  {
+  const [errorStatus, setErrorStatus] = useState('')
   const { data, error, isLoading, size, setSize } = useSWRInfinite(getKey, fetchPosts, {
     shouldRetryOnError: false
   })
-  console.log(isLoading)
+
+  console.log(error)
+
+  useEffect(() => {
+    if (error) setErrorStatus(String(error.response?.status))
+  }, [error])
+
   return (
     <>
       <ul className={styles.cardsList}>
 
-        {!data && !isLoading && (
+        {(!data && !isLoading && (errorStatus === '401')) && (
           <div role='alert' style={{textAlign: 'center'}}>
-            <p>Нет ни одного поста</p>
             <p>Для просмотра постов необходимо авторизоваться</p>
           </div>
         )}
 
+        {(!data && !isLoading && (errorStatus != '401')) && (
+          <div role='alert' style={{textAlign: 'center'}}>
+            <p>{error.message}</p>
+          </div>
+        )}
+
         {data?.map((posts) => {
+          if (posts.children.length === 0) return (
+            <div role='alert' style={{textAlign: 'center'}}>
+              <p>Нет ни одного поста</p>
+            </div>
+          )
+
           return posts.children.map((post: any) => (
             <Card
             key={post.data.id}
